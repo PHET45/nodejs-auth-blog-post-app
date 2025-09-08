1825efbd594f8c3330c567ba87840c3262f236bf
PHET45 <phnjk2000@gmail.com>
2025-09-08 14:22:46 +0700

    chore: update dependencies and enhance authentication context with JWT decoding

--- client/src/contexts/authentication.jsx
+++ client/src/contexts/authentication.jsx
@@ -1,35 +1,52 @@
-import React, { useState } from "react";
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import axios from 'axios'
+import { jwtDecode } from 'jwt-decode'
 
-const AuthContext = React.createContext();
+const AuthContext = React.createContext()
 
 function AuthProvider(props) {
   const [state, setState] = useState({
     loading: null,
     error: null,
     user: null,
-  });
+  })
 
-  const login = () => {
+  const navigate = useNavigate()
+
+  const login = async (data) => {
     // 🐨 Todo: Exercise #4
     //  ให้เขียน Logic ของ Function `login` ตรงนี้
     //  Function `login` ทำหน้าที่สร้าง Request ไปที่ API POST /login
     //  ที่สร้างไว้ด้านบนพร้อมกับ Body ที่กำหนดไว้ในตารางที่ออกแบบไว้
-  };
+    const result = await axios.post('http://localhost:4000/auth/login', data, {
+      headers: { 'Content-Type': 'application/json' },
+    })
+    const token = result.data.token
+    localStorage.setItem('token', token)
+    const userDataFromToken = jwtDecode(token)
+    setState({ ...state, user: userDataFromToken })
+    navigate('/')
+  }
 
-  const register = () => {
+  const register = async (data) => {
     // 🐨 Todo: Exercise #2
     //  ให้เขียน Logic ของ Function `register` ตรงนี้
     //  Function register ทำหน้าที่สร้าง Request ไปที่ API POST /register
     //  ที่สร้างไว้ด้านบนพร้อมกับ Body ที่กำหนดไว้ในตารางที่ออกแบบไว้
-  };
+    await axios.post('http://localhost:4000/auth/register', data, {
+      headers: { 'Content-Type': 'application/json' },
+    })
+    navigate('/login')
+  }
 
   const logout = () => {
     // 🐨 Todo: Exercise #7
     //  ให้เขียน Logic ของ Function `logout` ตรงนี้
     //  Function logout ทำหน้าที่ในการลบ JWT Token ออกจาก Local Storage
-  };
+  }
 
-  const isAuthenticated = Boolean(localStorage.getItem("token"));
+  const isAuthenticated = Boolean(localStorage.getItem('token'))
 
   return (
     <AuthContext.Provider
@@ -37,10 +54,10 @@ function AuthProvider(props) {
     >
       {props.children}
     </AuthContext.Provider>
-  );
+  )
 }
 
 // this is a hook that consume AuthContext
-const useAuth = () => React.useContext(AuthContext);
+const useAuth = () => React.useContext(AuthContext)
 
-export { AuthProvider, useAuth };
+export { AuthProvider, useAuth }
